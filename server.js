26fa97b5f933bd6da28f404fdb246c2ad8df7d9f
Alex Laursen <ajlaursen@aol.com>
2020-11-30 19:06:32 -0500

    changed line 13 from / to *

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
